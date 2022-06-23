62df63266b3cb73b4bbaef7d4927dae7a4b97e91
Dustin Yang <srdpt3@gmail.com>
2022-06-22 23:03:56 -0400

    update

---
+++ CryptoApp/Core/Home/Views/CoinRowView.swift
@@ -0,0 +1,36 @@
+//
+//  CoinRowView.swift
+//  CryptoApp
+//
+//  Created by Dustin yang on 6/22/22.
+//
+
+import SwiftUI
+
+struct CoinRowView: View {
+    let coin : CoinModel
+    var body: some View {
+        HStack(spacing: 0){
+            Text("\(coin.rank)").font(.caption).foregroundColor(Color.theme.secondaryText).frame(minWidth : 30)
+            
+            
+            Circle().frame(width: 30, height: 30)
+            Text(coin.symbol.uppercased()).font(.headline).padding(.leading, 6)
+                .foregroundColor(Color.theme.accent)
+            Spacer()
+            VStack(alignment : .trailing){
+                Text("\(coin.currentPrice)")
+                    .bold().foregroundColor(Color.theme.accent)
+                Text("\(coin.priceChangePercentage24H ?? 0)")
+                    .foregroundColor((coin.priceChangePercentage24H ?? 0) >= 0 ? Color.theme.green :Color.theme.red)
+                
+            }
+        }
+    }
+}
+
+struct CoinRowView_Previews: PreviewProvider {
+    static var previews: some View {
+        CoinRowView(coin: dev.coin)
+    }
+}
